f1c17c1558287f7508c1fd3a6ef6b6e75f6f5ec6
Zavistnichek <zavistnichek@gmail.com>
2025-01-31 10:53:15 +0500

    test

--- tests/test_bmi_print.py
+++ tests/test_bmi_print.py
@@ -2,12 +2,10 @@ import sys
 import os
 from day_01.bmi_calculator import BMICalculator
 
+# Adjust the path to point to the correct directory
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
 
+print("Current sys.path:", sys.path)
 
-def test_normal_bmi():
-    assert BMICalculator.calculate(70, 175) == 22.9
-
-
-def test_invalid_height():
-    assert BMICalculator.calculate(70, 0.4) is None
+bmi = BMICalculator.calculate(70, 175)
+print(bmi)
